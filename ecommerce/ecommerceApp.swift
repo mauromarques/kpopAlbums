836a7f45278347e304fc6bafd75418502532d9e7
Mauro <mauro.filho@ua.pt>
2021-07-20 09:33:29 +0100

    Initial Commit

---
+++ ecommerce/ecommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecommerceApp.swift
+//  ecommerce
+//
+//  Created by Mauro Canhao on 20/07/21.
+//
+
+import SwiftUI
+
+@main
+struct ecommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
